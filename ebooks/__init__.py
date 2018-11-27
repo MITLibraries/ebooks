08d053a66a9212361c738d9a63081cb63dabf16d
Helen Bailey <hakbailey@gmail.com>
2018-11-27 16:08:50 -0500

    Add Flask-Login for login routing, update Aleph query to use API key

--- ebooks/__init__.py
+++ ebooks/__init__.py
@@ -2,8 +2,10 @@ from __future__ import absolute_import
 
 from flask import Flask
 
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 import ebooks.queries
 import ebooks.views
--- ebooks/views.py
+++ ebooks/views.py
@@ -1,20 +1,47 @@
-from ebooks import app
-from flask import abort, render_template, send_file
-from ebooks.queries import get_file, get_filenames, get_metadata, get_volumes
+from flask import (abort, redirect, render_template,
+                   request, send_file, url_for)
+from flask_login import (current_user, login_required,
+                         login_user, LoginManager, logout_user, UserMixin)
+
+from werkzeug.urls import url_parse
+
+import os
 import requests
+
+from ebooks import app
+from ebooks.queries import get_file, get_filenames, get_metadata, get_volumes
+
+ALEPH_API_KEY = os.getenv('ALEPH_API_KEY')
+login = LoginManager(app)
+login.login_view = 'login'
+
+
+# Define the login route function here
+@app.route("/login")
+def login():
+    next_page = request.args.get('next')
+    if not next_page or url_parse(next_page).netloc != '':
+        next_page = url_for('index')
+    if current_user.is_authenticated:
+        return redirect(next_page)
+    # Log in user here and redirect to next_page
+    # Call authenticate function
+    # Create user variable that is instance of User class, with results of
+    #       authenticate function?
+    # Call login_user(user)
+    return("Login process to be implemented")
 
 
 @app.route("/")
 @app.route("/<item>")
+# @login_required
 def index(item="002341336"):
     files = get_filenames(item)
 
     metadata = {}
     try:
-        # record = requests.get("http://library.mit.edu/rest-dlf/record/mit01" +
-        #                       item + "?view=full")
-        record = requests.get("http://walter.mit.edu/rest-dlf/record/mit01" +
-                              item + "?view=full")
+        record = requests.get("https://library.mit.edu/rest-dlf/record/mit01" +
+                              item + "?view=full&key=" + ALEPH_API_KEY)
         marc_xml = record.content
         metadata = get_metadata(marc_xml)
     except:
@@ -34,6 +61,7 @@ def index(item="002341336"):
 
 
 @app.route('/docs/<filename>')
+@login_required
 def file(filename):
     if '..' in filename or filename.startswith('/'):
         abort(404)
