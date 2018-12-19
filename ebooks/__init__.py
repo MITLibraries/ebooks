2b98b9b64324633099a42c5c2a4c27e0949b8cd3
Helen Bailey <hakbailey@gmail.com>
2018-12-19 12:25:14 -0500

    Send exceptions to Sentry

--- ebooks/__init__.py
+++ ebooks/__init__.py
@@ -3,6 +3,13 @@ from __future__ import absolute_import
 from flask import Flask
 from flask_sslify import SSLify
 import os
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init(
+    dsn=os.getenv('SENTRY_DSN') or None,
+    integrations=[FlaskIntegration()]
+)
 
 app = Flask(__name__)
 app.config['ALEPH_API_KEY'] = os.getenv('ALEPH_API_KEY')
