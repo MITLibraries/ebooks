1fa9efe5671e52df37b9dfa88cae7bdbb63a9d09
jonavellecuerdo <cuerdojonavelle@gmail.com>
2024-07-15 13:17:24 -0400

    Update Ebooks app with new MIT branding
    Why these changes are being introduced:
    * The Institute recently updated its logo and branding guidelines, which
      we need to keep current.
    
    How this addresses that need:
    * Use new logos and favicon files from CDN and remove image files from repo
    * Add new 'Contact us' link
    * Add tool name to header
    * Apply consistent padding and width to logos
    * Remove “Search Hours & locations Borrow & request Research support About us” text/links
    * Add Docker compose yaml file for local IdP
    
    Side effects of this change:
    * Minimal updates were made to the HTML and CSS files, enough to
    render the expected header and footer in accordance with the updated
    branding guidelines. The code and syntax in these files can definitely
    be improved, but it is out of scope for this ticket.
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/PW-77

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,14 +3,14 @@ import os
 import boto3
 import pytest
 import requests_mock
-from moto import mock_s3
+from moto import mock_aws
 
 from ebooks import create_app
 
 
 @pytest.fixture
 def app():
-    os.environ['FLASK_ENV'] = 'testing'
+    os.environ["FLASK_ENV"] = "testing"
     app = create_app()
     yield app
 
@@ -19,49 +19,51 @@ def app():
 def client(app):
     with app.test_client() as client:
         with client.session_transaction() as sess:
-            sess['samlSessionIndex'] = 'exists'
+            sess["samlSessionIndex"] = "exists"
         return client
 
 
 @pytest.fixture()
 def s3_conn():
-    with mock_s3():
+    with mock_aws():
         conn = boto3.client(
-            's3',
-            aws_access_key_id='testing',
-            aws_secret_access_key='testing',
-            region_name='us-east-1'
-            )
-        conn.create_bucket(Bucket='samples')
-        conn.put_object(Bucket='samples',
-                        Key='sample_01-a.txt',
-                        Body='I am a file. I live in a bucket.',
-                        ContentType='text/plain')
-        conn.put_object(Bucket='samples',
-                        Key='sample_01-b.txt',
-                        Body='I am a second file. I also live in the bucket.')
-        conn.put_object(Bucket='samples',
-                        Key='has_volumes_a_2009.txt',
-                        Body='Vol 2009a')
-        conn.put_object(Bucket='samples',
-                        Key='has_volumes_b_2009.txt',
-                        Body='Vol 2009b')
-        conn.put_object(Bucket='samples',
-                        Key='sample.mp4',
-                        Body='fixtures/sample.mp4')
+            "s3",
+            aws_access_key_id="testing",
+            aws_secret_access_key="testing",
+            region_name="us-east-1",
+        )
+        conn.create_bucket(Bucket="samples")
+        conn.put_object(
+            Bucket="samples",
+            Key="sample_01-a.txt",
+            Body="I am a file. I live in a bucket.",
+            ContentType="text/plain",
+        )
+        conn.put_object(
+            Bucket="samples",
+            Key="sample_01-b.txt",
+            Body="I am a second file. I also live in the bucket.",
+        )
+        conn.put_object(
+            Bucket="samples", Key="has_volumes_a_2009.txt", Body="Vol 2009a"
+        )
+        conn.put_object(
+            Bucket="samples", Key="has_volumes_b_2009.txt", Body="Vol 2009b"
+        )
+        conn.put_object(Bucket="samples", Key="sample.mp4", Body="fixtures/sample.mp4")
         yield conn
 
 
 @pytest.fixture
 def record():
-    with open(_fixtures('sample.xml')) as f:
+    with open(_fixtures("sample.xml")) as f:
         record = f.read()
         return record
 
 
 @pytest.fixture
 def serial():
-    with open(_fixtures('serial.xml')) as f:
+    with open(_fixtures("serial.xml")) as f:
         record = f.read()
         return record
 
@@ -69,19 +71,26 @@ def serial():
 @pytest.fixture
 def alma(record, serial):
     with requests_mock.Mocker() as m:
-        m.get('https://mock.com/sample?apikey=',
-              status_code=200, content=record.encode())
-        m.get('https://mock.com/serial?apikey=',
-              status_code=200, content=serial.encode())
-        m.get('https://mock.com/fake_item?apikey=',
-              status_code=200,
-              content=('<?xmlversion = "1.0" encoding = ''"UTF-8"?>'
-                       '<get-record><reply-text>Record does not exist'
-                       '</reply-text><reply-code>0019</reply-code>'
-                       '</get-record>'.encode()))
+        m.get(
+            "https://mock.com/sample?apikey=", status_code=200, content=record.encode()
+        )
+        m.get(
+            "https://mock.com/serial?apikey=", status_code=200, content=serial.encode()
+        )
+        m.get(
+            "https://mock.com/fake_item?apikey=",
+            status_code=200,
+            content=(
+                '<?xmlversion = "1.0" encoding = '
+                '"UTF-8"?>'
+                "<get-record><reply-text>Record does not exist"
+                "</reply-text><reply-code>0019</reply-code>"
+                "</get-record>".encode()
+            ),
+        )
         yield m
 
 
 def _fixtures(path):
     current_dir = os.path.dirname(os.path.realpath(__file__))
-    return os.path.join(current_dir, 'fixtures', path)
+    return os.path.join(current_dir, "fixtures", path)
