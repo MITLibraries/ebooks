db9b0a0fa920a18e86b17850f91319f0395e199c
jonavellecuerdo <cuerdojonavelle@gmail.com>
2024-07-26 13:58:31 -0400

    Update Talisman configurations to load external resources
    Why these changes are being introduced:
    * While reviewing the MIT branding updates in production, a number
    of errors appeared in the console. The default Talisman content
    security policy configs are extremely strict and prevents loading
    any resources that are not in the same domain as the application.
    This resulted in the MIT images not rendering for the application
    (the images are loaded from our CDN). It also prevented running
    the script for Google Analytics.
    
    How this addresses that need:
    * Configure trusted domains for Talisman
    * Use 'nonce' to enable execution of in-line script for Google Analytics
    
    Side effects of this change:
    * Updated Content Security Policy for app
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/PW-77

--- ebooks/__init__.py
+++ ebooks/__init__.py
@@ -8,24 +8,41 @@ from sentry_sdk.integrations.flask import FlaskIntegration
 
 def create_app():
     sentry_sdk.init(
-        dsn=os.getenv('SENTRY_DSN') or None,
-        integrations=[FlaskIntegration()]
-        )
+        dsn=os.getenv("SENTRY_DSN") or None, integrations=[FlaskIntegration()]
+    )
 
     app = Flask(__name__, instance_relative_config=True)
 
-    flask_env = os.getenv('FLASK_ENV', default='production')
-    if flask_env == 'development':
-        app.config.from_object('ebooks.config.DevelopmentConfig')
-    elif flask_env == 'testing':
-        app.config.from_object('ebooks.config.TestingConfig')
+    flask_env = os.getenv("FLASK_ENV", default="production")
+    if flask_env == "development":
+        app.config.from_object("ebooks.config.DevelopmentConfig")
+    elif flask_env == "testing":
+        app.config.from_object("ebooks.config.TestingConfig")
     else:
-        app.config.from_object('ebooks.config.Config')
+        app.config.from_object("ebooks.config.Config")
 
     from ebooks import auth, item
+
     app.register_blueprint(auth.bp)
     app.register_blueprint(item.bp)
 
-    Talisman(app)
+    csp = {
+        "default-src": [
+            "'self'",
+            "*.mit.edu",
+            "*.mitlibrary.net",
+            "*.bootstrapcdn.com",
+            "*.cloudflare.com",
+            "*.google-analytics.com",
+            "*.googleapis.com",
+            "*.gstatic.com",
+            "*.jquery.com",
+        ]
+    }
+    nonce_list = ["default-src", "script-src"]
+
+    Talisman(
+        app, content_security_policy=csp, content_security_policy_nonce_in=nonce_list
+    )
 
     return app
